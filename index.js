683ae0cb9c020396afce3884eff945973c253b8c
unknown <elijahtinoet@gmail.com>
2021-09-09 09:04:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+  //my first javascript code!
+//  console. log('Hello World');
+//let name = 'tino'
+//console. log(name);
+//when we use let you can reassign variables
+ let interestRate = 0.3;
+ interestRate = 1;
+ console. log(interestRate);
+
+ //
+ 
